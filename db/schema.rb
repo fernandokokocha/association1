6ed33e8cb90fc0e473db96a36598c9a83deb3134
Fernando <krajka.bartosz@gmail.com>
2014-10-26 21:50:14 +0100

    Changed to Porstges

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141026193910) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "order_lines", force: true do |t|
     t.integer  "order_id"
     t.integer  "product_id"
